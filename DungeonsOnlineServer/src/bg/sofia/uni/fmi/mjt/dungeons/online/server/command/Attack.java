2522fa44d74afafbaa09cb58665a8c72857c23b6
Georgi Mihaylov <georgimihaylov2002@gmail.com>
2024-02-13 00:49:04 +0200

    refactor

--- DungeonsOnlineServer/src/bg/sofia/uni/fmi/mjt/dungeons/online/server/command/Attack.java
+++ DungeonsOnlineServer/src/bg/sofia/uni/fmi/mjt/dungeons/online/server/command/Attack.java
@@ -17,26 +17,12 @@ public class Attack {
         int attackerNumber = map.getPlayerNumber(id);
         int attackStat = attacker.getStats().getAttack();
 
-        List<Player> playersOnPosition = map.getPlayersOnPosition(position);
-
         CommandResponse response = new CommandResponse();
 
         int targetsHit = 0;
-        for (Player defender : playersOnPosition) {
-            if (defender.equals(attacker)) {
-                continue;
-            }
-            defender.loseHealth(attackStat);
-            response.addResponse(defender.getId(), "You were attacked by " + attackerNumber
-                + "! You lose " + attackStat + " health!\n");
-            targetsHit++;
-        }
+        targetsHit += hitPlayers(map, position, attacker, attackStat, response, attackerNumber);
 
-        if (map.isMinionOnPosition(position)) {
-            Minion minion = map.getMinionOnPosition(position);
-            minion.loseHealth(attackStat);
-            targetsHit++;
-        }
+        targetsHit += hitMinion(map, position, attackStat);
 
         if (targetsHit == 0) {
             return CommandResponse.of(id, "You did not hit anything!\n");
@@ -44,5 +30,32 @@ public class Attack {
 
         response.addResponse(id, "Attack successful, targets hit: " + targetsHit + "\n");
         return response;
+    }
+
+    private static int hitPlayers(DungeonMap map, Position position, Player attacker, int attackStat,
+                                     CommandResponse response, int attackerNumber) {
+        List<Player> playersOnPosition = map.getPlayersOnPosition(position);
+        int hits = 0;
+
+        for (Player defender : playersOnPosition) {
+            if (defender.equals(attacker)) {
+                continue;
+            }
+            defender.loseHealth(attackStat);
+            response.addResponse(defender.getId(), "You were attacked by " + attackerNumber
+                + "! You lose " + attackStat + " health!\n");
+            hits++;
+        }
+        return hits;
+    }
+
+    private static int hitMinion(DungeonMap map, Position position, int attackStat) {
+        int hit = 0;
+        if (map.isMinionOnPosition(position)) {
+            Minion minion = map.getMinionOnPosition(position);
+            minion.loseHealth(attackStat);
+            hit++;
+        }
+        return hit;
     }
 }
